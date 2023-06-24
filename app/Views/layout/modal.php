d248a4760da4589ab7ef9581f23a0d2acf7440c8
Alan1252 <alankamesta.ginting@gmail.com>
2023-06-24 19:11:48 +0700

    modal

---
+++ app/Views/layout/modal.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <style>
+
+    </style>
+</head>
+
+<body>
+
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="modal-box">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <div class="modal-icon">
+                                        <i style="margin-top: 8px;" class="fa fa-frown"></i>
+                                    </div>
+                                    <h3 class="title">Ooops!</h3>
+                                    <p class="description">Akses login dibutukan<br> Login untuk melanjutkan.</p>
+                                    <a href="/home">
+                                        <button class="btn" style="background:tomato">Home</button>
+                                    </a>
+                                    <a href="/login">
+                                        <button class="btn">Login</button>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Add Bootstrap and jQuery scripts here -->
+
+</body>
+
+</html>
